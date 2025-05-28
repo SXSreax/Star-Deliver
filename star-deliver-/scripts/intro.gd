67b40726d58012f8115e0f14b878a2c8a4379a6d
24755-Kyle <24755@burnside.school.nz>
2025-05-29 10:21:00 +1200

    update assets intro
    Intro basic coding and slides completed, assets of map, pain add

--- star-deliver-/scripts/intro.gd
+++ star-deliver-/scripts/intro.gd
@@ -1,10 +1,35 @@
 extends Node2D
 
+@onready var sprite = $Sprite2D
+var image_paths = [
+	"res://assets/Intro Slides/01.png",
+	"res://assets/Intro Slides/02.png",
+	"res://assets/Intro Slides/03.png",
+	"res://assets/Intro Slides/04.png",
+	"res://assets/Intro Slides/05.png",
+	"res://assets/Intro Slides/06.png"
+]
+
+var current_index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("fade in ")
+	_start_slideshow()
 
+func _start_slideshow():
+	await get_tree().create_timer(1.5).timeout  # ⏳ One-time delay before loop starts
+	_update_sprite()
+	_start_timer()
+	
+func _update_sprite():
+	$Slide.texture = load(image_paths[current_index])
+
+func _start_timer():
+	await get_tree().create_timer(7.25).timeout
+	current_index = (current_index + 1) % image_paths.size()
+	_update_sprite()
+	_start_timer()  # repeat loop
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
