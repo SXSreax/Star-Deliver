2c671e66df48732da62e25a9b5e606eda6ed4131
G.A.P Vidunitha <158988533+AlgorithmicPV@users.noreply.github.com>
2025-06-11 10:10:45 +1200

    Complete the chasing system for crab [one of the enemy in the second planet]

--- star-deliver-/scripts/killer_rabbit.gd
+++ star-deliver-/scripts/killer_rabbit.gd
@@ -1,28 +1,3 @@
-#extends CharacterBody2D
-#
-#
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-#
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
 extends CharacterBody2D
 
 @onready var player = $"../Player"
@@ -33,16 +8,13 @@ var speed = 100
 func _physics_process(delta: float) -> void:
 	var direction = (player.global_position - raycast.global_position).normalized()
 	var length = 500  # Set your desired ray length here
-	raycast.target_position = direction * length
-
+	raycast.target_position = direction * length 
 	
 	if raycast.is_colliding(): 
 		#print("chase")
 		var collider = raycast.get_collider()
-		var collider_name = collider.name
-		#print("rabiit is detecting ",collider_name)
+		var collider_name = collider.name # Gets the collider name
+		# If the colider name is equal to the player, moves
 		if collider_name == "Player":
 			velocity = direction * speed
 			move_and_slide()
-	#else:
-		#print("dont chase")
