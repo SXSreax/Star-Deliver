a39dbe0d0b740b7c9014d0527a5d0046d652a95c
G.A.P Vidunitha <158988533+AlgorithmicPV@users.noreply.github.com>
2025-06-23 16:26:03 +1200

    Implement directional animations and item-based idle/walk states
    Added animation logic to respond to Hotbar selection for sword and package. Character now plays correct idle and walking animations based on movement direction and current item held (sword or package).

--- star-deliver-/scripts/hotbar.gd
+++ star-deliver-/scripts/hotbar.gd
@@ -1,6 +1,6 @@
 extends Control
-@onready var animated_sprite: AnimatedSprite2D = $"../../AnimatedSprite2D"
 @onready var slots = get_children()
+@onready var player: AnimatedSprite2D = $"../../player"
 
 var current_index : int:
 	set(value):
@@ -22,17 +22,17 @@ func set_focus():
 	get_child(current_index).set_process_input(true)
 	
 func _input(event):
-	if event.is_action_pressed("inventory"):
+	if event.is_action_pressed("scroll_down"):
 		if current_index == get_child_count() - 1:
-			current_index = 0
-			
+			current_index = 0		
 		else:
 			current_index += 1
-		if current_index == 1:
-			animated_sprite.play("sword")
-		if current_index == 2:
-			animated_sprite.play("package")
-		if current_index == 3:
-			animated_sprite.play("gun")
-		if current_index == 0:
-			animated_sprite.play("default")
+	if event.is_action_pressed("1"):
+		current_index = 0
+	if event.is_action_pressed("2"):
+		current_index = 1
+	if event.is_action_pressed("3"):
+		current_index = 2
+	if event.is_action_pressed("4"):
+		current_index = 3
+	
--- star-deliver-/scripts/player.gd
+++ star-deliver-/scripts/player.gd
@@ -1,11 +1,104 @@
 extends CharacterBody2D
 @onready var hotbar: HBoxContainer = $UI/Hotbar
-
+@onready var player: AnimatedSprite2D = $player
+@onready var slots = $UI/Hotbar.get_children()
 @export var speed = 400
+var last_direction =  "right up"
+
 
 func get_input():
 	var input_direction = Input.get_vector("left","right","up","down")
+	var selected_slot = hotbar.current_index
+	print(selected_slot)
+	# Get the number , pass that into movement , depende on that change the animation, and add a colide area detection detect detect enemeues and if there are enmeies, print out
 	velocity = input_direction * speed
+	if input_direction == Vector2.ZERO:
+		if last_direction == "right up":
+			if selected_slot == 1:
+				player.play("idle spear right up")
+			else:
+				player.play("idle right up")
+		elif last_direction == "right down":
+			if selected_slot == 1:
+				player.play("idle spear right down")
+			else:
+				player.play("idle right down")
+		elif last_direction == "left up":
+			if selected_slot == 1:
+				player.play("idle spear left up")
+			else:
+				player.play("idle left up")
+		elif last_direction == "left down":
+			if selected_slot == 1:
+				player.play("idle spear left down")
+			else:
+				player.play("idle left down")
+		elif last_direction == "right down":
+			if selected_slot == 1:
+				player.play("idle spear right down")
+			else:
+				player.play("idle right down")
+		elif last_direction == "up":
+			if selected_slot == 1:
+				player.play("idle spear up")
+			else:
+				player.play("idle up")
+		else:
+			if selected_slot == 1:
+				player.play("idle spear down")
+			else:
+				player.play("idle down")
+	elif input_direction.x > 0 and input_direction.y < 0:
+		if selected_slot == 1:
+			player.play("spear walk right up")
+		else:
+			player.play("walk right up")
+			#pass
+		last_direction = "right up"
+	elif input_direction.x > 0 and input_direction.y > 0:
+		if selected_slot == 1:
+			player.play("spear walk right down")
+		else:
+			player.play("walk right down")
+		last_direction = "right down"
+	elif input_direction.x < 0 and input_direction.y < 0:
+		if selected_slot == 1:
+			player.play("spear walk left up")
+		else:
+			player.play("walk left up")#
+		last_direction = "left up"
+	elif input_direction.x < 0 and input_direction.y > 0:
+		if selected_slot == 1:
+			player.play("spear walk left down")
+		else:
+			player.play("walk left down")#
+		last_direction = "left down"
+	elif input_direction.x > 0:
+		if selected_slot == 1:
+			player.play("spear walk right down")
+		else:
+			player.play("walk right down")#
+		last_direction = "right down"
+	elif input_direction.x < 0:
+		if selected_slot == 1:
+			player.play("spear walk left down")
+		else:
+			player.play("walk left down")#
+		last_direction = "left down"
+	elif input_direction.y > 0:
+		if selected_slot == 1:
+			player.play("spear walk down")
+		else:
+			player.play("walk down")
+		last_direction = "down"
+		
+	elif input_direction.y < 0:
+		if selected_slot == 1:
+			player.play("spear walk up")
+		else:
+			player.play("walk up")
+		last_direction = "up"
+
 
 func _physics_process(delta: float) -> void:
 	get_input()
